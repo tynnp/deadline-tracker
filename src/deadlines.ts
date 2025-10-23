57fa51552ed86618169cecb96f89fc59314eb412
tynnp <nguyenngocphuty20172@laivung2.edu.vn>
2025-10-23 19:08:45 +0700

    Thêm trạng thái

--- src/deadlines.ts
+++ src/deadlines.ts
@@ -3,6 +3,7 @@ export interface Deadline {
   title: string;
   date: string;
   time: string;
+  done: boolean;
 }
 
 export const deadlines: Deadline[] = [
@@ -11,17 +12,27 @@ export const deadlines: Deadline[] = [
     title: 'Cuối kỳ Công nghệ phần mềm',
     date: '2025-10-31',
     time: '23:59',
+    done: false,
   },
   {
     id: '2',
     title: 'Giữa kỳ Lập trình PHP',
     date: '2025-12-26',
     time: '23:59',
+    done: false,
   },
   {
     id: '3',
     title: 'Cuối kỳ Lập trình PHP',
     date: '2026-01-09',
     time: '23:59',
+    done: false,
   },
+  {
+    id: '4',
+    title: 'Làm VLE sinh hoạt công dân',
+    date: '2025-10-23',
+    time: '23:59',
+    done: true,
+  }
 ];
